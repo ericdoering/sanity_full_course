d857f78cce76ff0c3286e232e9ec08b2f5883d29
Eric Doering <ericbenjamindoering@gmail.com>
2025-09-04 14:41:22 -0400

    deployment and new schema details added to eventType

--- schemaTypes/eventType.ts
+++ schemaTypes/eventType.ts
@@ -4,45 +4,81 @@ export const eventType = defineType({
     name: "event",
     title: "Event",
     type: "document",
+    groups: [
+        {name: "details", title: "Details"},
+        {name: "editorial", title: "Editorial"},
+    ],
     fields: [
         defineField({
             name: "name",
-            type: "string"
+            type: "string",
+            group: "details",
         }),
         defineField({
             name: "slug",
-            type: "slug"
+            type: "slug",
+            group: "details",
+            options: {
+                source: "name"
+            },
+            validation: (rule) => rule.required().error("Required to generate a page on the website."),
+            hidden: ({document}) => !document?.name,
+        }),
+        defineField({
+            name: "eventType",
+            type: "string",
+            group: "details",
+            options: {
+                list: ["in-person", "virtual"],
+                layout: "radio"
+            }
         }),
         defineField({
             name: "date",
-            type: "datetime"
+            type: "datetime",
+            group: "details",
         }),
         defineField({
             name: "doorsOpen",
-            type: "number"
+            description: "Number of minutes before the start time of admission.",
+            type: "number",
+            group: "details",
+            initialValue: 60,
         }),
         defineField({
             name: "venue",
             type: "reference",
-            to: [{type: "venue"}]
+            group: "details",
+            to: [{type: "venue"}],
+            readOnly: ({value, document}) => !value && document?.eventType === "virtual",
+            validation: (rule) => rule.custom((value, context) => {
+                if(value && context?.document?.eventType === "virtual"){
+                    return "Only in-person events can have a venue."
+                }
+                return true
+            }),
         }),
         defineField({
             name: "headline",
             type: "reference",
+            group: "editorial",
             to: [{type: "artist"}]
         }),
         defineField({
             name: "image",
             type: "image",
+            group: "editorial",
         }),
         defineField({
             name: "details",
             type: "array",
+            group: "editorial",
             of: [{type: "block"}]
         }),
         defineField({
             name: "tickets",
             type: "url",
+            group: "editorial",
         }),
     ],
 })
